9003ec79106d7f8babb56bf691687846c61f3d83
Sener DEMIRAL <sener.demiral@outlook.com>
2024-01-21 14:19:28 +0300

    21.01.24

--- YapHaydi/DataLibrary/OO.cs
+++ YapHaydi/DataLibrary/OO.cs
@@ -8,33 +8,43 @@ public class OO
 	public int ReqID;
 	public int ActID;
 
-	public DateTime? RQD;
 
-	public DateTime? ESD;
-	public DateTime? ESDt = DateTime.Today;
+	public DateTime? RSD;
+	public DateTime? RSDt = DateTime.Today;
 	public DateTime? ASD;
 
-	public DateTime? EFD;
-	public DateTime? EFDt = DateTime.Today;
+	public DateTime? RFD;
+	public DateTime? RFDt = DateTime.Today;
 	public DateTime? AFD;
-
 	public string? Inf;
 	public string? Msj;
 
+	public int InsID;
+	public int UpdID;
+	public DateTime? InsTS;
+	public DateTime? UpdTS;
+
 	public int sdh;
 	public int fdh;
-
 
 	public string Sbj;
 	public string Req;
 	public string Act;
 
-	public string? RQDfs => RQD?.ToString("dd.MM.yy HH:mm");
-	public string? ESDfs => ESD?.ToString("dd.MM.yy HH:mm");
+	public string? RSDfs => RSD?.ToString("dd.MM.yy HH:mm");
 	public string? ASDfs => ASD?.ToString("dd.MM.yy HH:mm");
-	public string? EFDfs => EFD?.ToString("dd.MM.yy HH:mm");
+	public string? RFDfs => RFD?.ToString("dd.MM.yy HH:mm");
 	public string? AFDfs => AFD?.ToString("dd.MM.yy HH:mm");
+	public bool Started => ASD != null;
+	public bool Finishhed => AFD != null;
+	
+	public string InsUsr;
+	public string UpdUsr;
+	public string? InsTSfs => InsTS?.ToString("dd.MM.yy HH:mm");
+	public string? UpdTSfs => UpdTS?.ToString("dd.MM.yy HH:mm");
 
+	public bool UpdatePending = false;
+	public int UpdateSoF;   // 1:Started, 2:Finished
 
 	public OO ShallowCopy()
 	{
